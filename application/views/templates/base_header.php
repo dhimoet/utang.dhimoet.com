a6e744581c28a692a1af2a75f39add423e4c61d9
Adhika Mahendra <dhimoet@gmail.com>
2012-08-12 23:07:41 -0400

    added add2home.js

--- application/views/templates/base_header.php
+++ application/views/templates/base_header.php
@@ -9,12 +9,14 @@
 	
 	<link rel="stylesheet" type="text/css" href="/static/css/style.css" />
 	<link rel="stylesheet" type="text/css" href="/static/css/validationEngine.jquery.css" />
+	<link rel="stylesheet" type="text/css" href="/static/css/add2home.css" />
 	<link rel="stylesheet" type="text/css" href="/static/jquery.mobile-1.1.1/jquery.mobile-1.1.1.min.css" />
 	<script type="text/javascript" src="/static/js/jquery-1.7.2.min.js"></script>
 	<script type="text/javascript" src="/static/js/jquery.validationEngine.js"></script>
 	<script type="text/javascript" src="/static/js/jquery.validationEngine-en.js"></script>
 	<script type="text/javascript" src="/static/js/underscore-min.js"></script>
 	<script type="text/javascript" src="/static/js/backbone-min.js"></script>
+	<script type="text/javascript" src="/static/js/add2home.js"></script>
 	<script type="text/javascript" src="/static/jquery.mobile-1.1.1/jquery.mobile-1.1.1.min.js"></script>
 	<script type="text/javascript" src="/static/js/script.js"></script>
 
