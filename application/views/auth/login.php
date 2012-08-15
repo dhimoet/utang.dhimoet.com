0978befd6d8052dd625318cf24f04dc9ea427cb0
Adhika Mahendra <dhimoet@gmail.com>
2012-08-14 21:33:18 -0400

    completed

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -7,36 +7,14 @@
   
 	<div data-role='content'>
 		
-		<div class="bottom_40">
+		<div class="bottom_40 top_40">
 			
 			<a href='/fb/login/' data-role='button' data-theme='b' data-ajax='false'>Login with Facebook</a>
 			
 		</div>
 	
 		<hr />
-		
-		<div>
-			
-			<form name='login' action='/auth/login' method='post' data-ajax='false'>
-	 
-				<p>
-					<label for="identity">Email:</label>
-					<input type='text' name='identity' value='' class="validate[required,custom[email]]"/>
-				</p>
-			  
-				<p>
-					<label for="password">Password:</label>
-					<input type='password' name='password' value='' class="validate[required]"/>
-				</p>
-			  
-				<label><input type="checkbox" name="remember" /> Remember Me </label>
-				  
-				<input type='submit' name='login' value='Login' data-theme='e'/>
-			  
-			</form>
-			
-		</div>
-	
+
 	</div><!-- content container -->
   
 	<div data-role='header' style='position:fixed; bottom:0px;'></div>
