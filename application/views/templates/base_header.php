004b90c12a26c9841c1062d7b916488fa2152a13
Adhika Mahendra <amahendra@ubuntu.(none)>
2012-08-15 16:31:03 -0400

    added startup image

--- application/views/templates/base_header.php
+++ application/views/templates/base_header.php
@@ -9,7 +9,7 @@
 	
 	<link href="/static/img/utangapp_web_logo.png" sizes="114x114" rel="apple-touch-icon">
 	<link rel="apple-touch-startup-image" media="(device-width: 320px) and (-webkit-device-pixel-ratio: 2)"
-		href="utangapp_web_image.png">
+		href="/static/img/utangapp_web_image.png">
       
 	<link rel="stylesheet" type="text/css" href="/static/css/style.css" />
 	<link rel="stylesheet" type="text/css" href="/static/css/validationEngine.jquery.css" />
